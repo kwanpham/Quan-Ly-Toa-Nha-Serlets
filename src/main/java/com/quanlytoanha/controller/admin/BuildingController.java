3eec2e52c1148736fdaef98ac354e8b47f5ea576
K.Wan <chomchomcapital@gmail.com>
2019-02-18 21:37:18 +0700

    comit

--- src/main/java/com/quanlytoanha/controller/admin/BuildingController.java
+++ src/main/java/com/quanlytoanha/controller/admin/BuildingController.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = {"/admin-add-building"})
+@WebServlet(urlPatterns = {"/admin-building"})
 public class BuildingController extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
