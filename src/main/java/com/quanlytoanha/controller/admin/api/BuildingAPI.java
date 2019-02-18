56389aa9968505008447c369322e2881f8d92a8d
K.Wan <chomchomcapital@gmail.com>
2019-02-18 21:37:41 +0700

    comit

--- src/main/java/com/quanlytoanha/controller/admin/api/BuildingAPI.java
+++ src/main/java/com/quanlytoanha/controller/admin/api/BuildingAPI.java
@@ -1,6 +1,11 @@
 package com.quanlytoanha.controller.admin.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mysql.cj.xdevapi.Type;
+import com.quanlytoanha.model.BuildingModel;
 import com.quanlytoanha.service.IBuildingService;
+import com.quanlytoanha.utils.HttpUtil;
+
 
 import javax.inject.Inject;
 import javax.servlet.ServletException;
@@ -10,20 +15,29 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-/**
- * Created by MyPC on 18/02/2019.
- */
 @WebServlet(urlPatterns = {"/api-admin-building"})
 public class BuildingAPI extends HttpServlet {
 
     @Inject
     private IBuildingService buildingService;
 
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        ObjectMapper objectMapper = new ObjectMapper();
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+        BuildingModel buildingModel = HttpUtil.of(request.getReader()).toModel(BuildingModel.class);
+        buildingModel = buildingService.save(buildingModel);
+        objectMapper.writeValue(response.getOutputStream() , buildingModel);
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        req.setCharacterEncoding("UTF-8");
+        resp.setContentType("application/json");
+        BuildingModel buildingModel = HttpUtil.of(req.getReader()).toModel(BuildingModel.class);
+        buildingModel = buildingService.update(buildingModel);
+        mapper.writeValue(resp.getOutputStream() , buildingModel);
     }
 }
